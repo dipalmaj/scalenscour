9fc26f04c9e54eddaf120f108a92139ab38ab776
Gio DiPalma <dipalmaj@users.noreply.github.com>
2017-12-01 19:25:02 -0500

    Added notes to Ch3

--- src/main/scala/fpinscala/datastructures/List.scala
+++ src/main/scala/fpinscala/datastructures/List.scala
@@ -8,6 +8,10 @@ which may be `Nil` or another `Cons`.
 case class Cons[+A](head: A, tail: List[A]) extends List[A]
 
 object List { // `List` companion object. Contains functions for creating and working with lists.
+
+  /*
+  Provided Implementations
+   */
   def sum(ints: List[Int]): Int = ints match { // A function that uses pattern matching to add up a list of integers
     case Nil => 0 // The sum of the empty list is 0.
     case Cons(x,xs) => x + sum(xs) // The sum of a list starting with `x` is `x` plus the sum of the rest of the list.
@@ -22,14 +26,6 @@ object List { // `List` companion object. Contains functions for creating and wo
   def apply[A](as: A*): List[A] = // Variadic function syntax
     if (as.isEmpty) Nil
     else Cons(as.head, apply(as.tail: _*))
-
-  val x = List(1,2,3,4,5) match {
-    case Cons(x, Cons(2, Cons(4, _))) => x
-    case Nil => 42
-    case Cons(x, Cons(y, Cons(3, Cons(4, _)))) => x + y
-    case Cons(h, t) => h + sum(t)
-    case _ => 101
-  }
 
   def append[A](a1: List[A], a2: List[A]): List[A] =
     a1 match {
@@ -49,15 +45,31 @@ object List { // `List` companion object. Contains functions for creating and wo
   def product2(ns: List[Double]) =
     foldRight(ns, 1.0)(_ * _) // `_ * _` is more concise notation for `(x,y) => x * y`; see sidebar
 
+  /*
+   Exercises
+   */
 
+  // Exercise 3.1
+  val x = List(1,2,3,4,5) match {
+    case Cons(x, Cons(2, Cons(4, _))) => x
+    case Nil => 42
+    case Cons(x, Cons(y, Cons(3, Cons(4, _)))) => x + y
+    case Cons(h, t) => h + sum(t)
+    case _ => 101
+  }
+
+  // Exercise 3.2
   def tail[A](l: List[A]): List[A] = ???
 
+  // Exercise 3.3
   def setHead[A](l: List[A], h: A): List[A] = ???
 
+  // Exercise 3.4
   def drop[A](l: List[A], n: Int): List[A] = ???
 
+  // Exercise 3.5
   def dropWhile[A](l: List[A], f: A => Boolean): List[A] = ???
-
+  
   def init[A](l: List[A]): List[A] = ???
 
   def length[A](l: List[A]): Int = ???
