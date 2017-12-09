fca7fd7fe822ec26305bd62466f125b7970e982c
Gio DiPalma <dipalmaj@users.noreply.github.com>
2017-12-09 13:24:30 -0500

    Some implementations

--- src/main/scala/fpinscala/datastructures/List.scala
+++ src/main/scala/fpinscala/datastructures/List.scala
@@ -34,9 +34,8 @@ object List { // `List` companion object. Contains functions for creating and wo
     }
 
   def foldRight[A,B](as: List[A], z: B)(f: (A, B) => B): B = // Utility functions
-    as match {
-      case Nil => z
-      case Cons(x, xs) => f(x, foldRight(xs, z)(f))
+    {
+      foldLeft(as, z)((d: B, e: A) => f(e,d))
     }
 
   def sum2(ns: List[Int]) =
@@ -50,7 +49,7 @@ object List { // `List` companion object. Contains functions for creating and wo
    */
 
   // Exercise 3.1
-  val x = List(1,2,3,4,5) match {
+  val z = List(1,2,3,4,5) match {
     case Cons(x, Cons(2, Cons(4, _))) => x
     case Nil => 42
     case Cons(x, Cons(y, Cons(3, Cons(4, _)))) => x + y
@@ -74,13 +73,53 @@ object List { // `List` companion object. Contains functions for creating and wo
   }
 
   // Exercise 3.5
-  def dropWhile[A](l: List[A], f: A => Boolean): List[A] = ???
-  
-  def init[A](l: List[A]): List[A] = ???
+  def dropWhile[A](l: List[A], f: A => Boolean): List[A] = l match  {
+    case Cons(x, xs) if f(x) => dropWhile(xs, f)
+    case _ => l
+  }
 
-  def length[A](l: List[A]): Int = ???
+  // Ex 3.6
+  def init[A](l: List[A]): List[A] = l match {
+    case Cons(_, Nil) => Nil
+    case Cons(x, xs) => Cons(x, init(xs))
+    case _ => Nil
+  }
 
-  def foldLeft[A,B](l: List[A], z: B)(f: (B, A) => B): B = ???
+  def length[A](l: List[A]): Int = foldRight(l,0)((_,y) => y + 1)
 
-  def map[A,B](l: List[A])(f: A => B): List[B] = ???
+  def foldLeft[A,B](l: List[A], z: B)(f: (B, A) => B): B = l match {
+    case Cons(x, xs) => foldLeft(xs, f(z,x))(f)
+    case _ => z
+  }
+
+  def map[A,B](l: List[A])(f: A => B): List[B] = l match {
+    case Cons(x, xs) => foldLeft(xs, Cons(f(x), Nil))((z,y) => Cons(f(y),z))
+    case _ => Nil
+  }
+}
+
+
+object Test {
+  import List._
+
+  def main(args: Array[String]): Unit = {
+    val test = Cons(3, Cons(2, Cons(1, Nil)))
+    println(init(test))
+    println(init(init(test)))
+
+    println(map(test)(_ * 2))
+
+    foldLeft(test, 0)( (b,a) => {
+      val c = b+a
+      println(c)
+      c
+    })
+
+    println("Right")
+    foldRight(test, 0)( (a,b) => {
+      val c = b+a
+      println(c)
+      c
+    })
+  }
 }
